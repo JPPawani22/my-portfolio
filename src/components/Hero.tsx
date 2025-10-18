0b793e9ca300b532d9d46a8ab4d3c2458e1bc487
Pawani <pawani02jp@gmail.com>
2025-10-19 02:57:38 +0530

    update CV

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -76,7 +76,7 @@ export default function Hero() {
                 Get In Touch
               </button>
               <a 
-                href="https://drive.google.com/file/d/1t5sc5NFtCh_XoE7UHxguJ8nmqnlyBkB6/view?usp=sharing" 
+                href="https://drive.google.com/file/d/1Xksp_sSRe1PwegMRLTQdDwAron8pR41Q/view?usp=sharing" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className={styles.btnPrimary}
